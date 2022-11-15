c86ee66efe1796a3a634284e744f1a529f275846
Stephanie <slee185@uncc.edu>
2022-11-15 12:03:33 -0500

    create DatabaseHelper

---
+++ app/src/main/java/edu/uncc/inclass12/DatabaseHelper.java
@@ -0,0 +1,29 @@
+// In Class Assignment 12
+// Group22_InClass12
+// Stephanie Lee Karp & Ken Stanley
+
+package edu.uncc.inclass12;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+
+    public DatabaseHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
