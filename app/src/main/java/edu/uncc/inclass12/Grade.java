7ffd1a632a69195f7d0628952e45480552c13c61
Stephanie <slee185@uncc.edu>
2022-11-14 20:58:52 -0500

    Initial commit

---
+++ app/src/main/java/edu/uncc/inclass12/Grade.java
@@ -0,0 +1,4 @@
+package edu.uncc.inclass12;
+
+public class Grade {
+}
---
+++ app/src/main/java/edu/uncc/inclass12/MainActivity.java
@@ -0,0 +1,18 @@
+package edu.uncc.inclass12;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.rootView, new GradesFragment())
+                .commit();
+    }
+}
